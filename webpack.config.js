c5ecfd5e062a59612f560f913f4855644b59c97f
Gavin Grant <gavingrant@gmail.com>
2023-02-07 17:32:11 -0800

    refactor: webpack output

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,8 @@ module.exports = {
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "build"),
+    publicPath: "/",
+    filename: "bundle.js",
   },
   plugins: [
     new HtmlWebpackPlugin({
